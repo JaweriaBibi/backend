4c839a7307784b30abc40524ddf27446b0705786
jaweria bibi abdullah <jaweriabibi4@gmail.com>
2025-01-30 21:31:51 +0500

    working on post and edit method

--- REST_API/index.js
+++ REST_API/index.js
@@ -6,6 +6,14 @@ const port = 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
 
+
+const methodoverride = require('method-override');
+app.use(methodoverride('_method'));
+
+
+const { v4: uuidv4 } = require('uuid');
+uuidv4();
+
 const path = require("path");
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -14,19 +22,19 @@ app.use(express.static(path.join(__dirname, "public")));
 
 let posts = [
     {
-        id: 1,
+        id: uuidv4(),
         username : 'John Doe',
         title: 'First Post',
         content: 'This is the first post',
     },
     {
-        id: 2,
+        id: uuidv4(),
         username : 'Jane Doe',
         title: 'Second Post',
         content: 'This is the second post',
     },
     {
-        id: 3,
+        id: uuidv4(),
         username : 'John Doe',
         title: 'Third Post',
         content: 'This is the third post',
@@ -38,17 +46,44 @@ app.get('/post', (req, res) => {
     res.render('post', {posts: posts});
 })
 
-// app.get('/post/new', (req, res) => {
-//     res.render('newPost');
-// })
+app.get('/post/new', (req, res) => {
+    res.render('postcreate');
+})
 
 app.get('/post/:id', (req, res) => {
     const { id } = req.params;
-    console.log(id)
-    const post = posts.find(post => post.id === parseInt(id));
-    console.log(post);
+    // console.log(id)
+    const post = posts.find(post => post.id === id);
+    // console.log(post);
     res.render('postDetail', {post : post});
 })
+
+app.post('/post', (req, res) => {
+    let { title, content, username } = req.body;
+    let id = uuidv4();
+    posts.push({id, title, content, username});
+    res.redirect('/post');
+})
+
+
+app.patch('/post/:id', (req, res) => {
+    const { id } = req.params;
+    const { title, content } = req.body;
+    const post = posts.find(post => post.id === id);
+    post.title = title;
+    post.content = content;
+    res.redirect('/post');
+})
+
+
+app.get('/post/:id/edit', (req, res) => {
+    const { id } = req.params;
+    const post = posts.find(post => post.id === id);
+    res.render('postedit', {post : post});
+})
+
+
+
 
 
 app.listen(port, () => {
